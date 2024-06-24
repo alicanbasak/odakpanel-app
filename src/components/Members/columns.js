eb298e4f06794af6fd11c6d2151052b5d0caa62d
Alican Basak <alicanbasak@icloud.com>
2024-06-24 05:21:10 +0300

    implement new columns

--- src/components/Members/columns.js
+++ src/components/Members/columns.js
@@ -1,32 +1,3 @@
-// import { setStandartColumn } from "../../utils/setStandartColumn";
-// const fields = [
-//   {
-//     field: "Id",
-//     headerName: "Id",
-//     valueGetter: params => params.row.Id,
-//     sortable: true,
-//   },
-//   {
-//     field: "Email",
-//     headerName: "Email",
-//     sortable: true,
-//     flex: 1,
-//   },
-//   {
-//     field: "Name",
-//     headerName: "Name",
-//     sortable: true,
-//     flex: 1,
-//   },
-//   {
-//     field: "Surname",
-//     headerName: "Surname",
-//     sortable: true,
-//     flex: 1,
-//   },
-// ];
-// export const columns = setStandartColumn(fields);
-
 import { createColumnConfig } from "../../utils/columnCreator";
 
 export const baseColumns = [
